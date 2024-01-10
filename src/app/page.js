14591049cf8a9e8919d4d2ba52410b4330069e55
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-10 16:37:12 +0700

    feat: add layout background color

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ export default async function Home() {
   return (
     <div className="grid grid-cols-4 gap-6 h-[calc(100vh-220px)]">
       {/* LEFT */}
-      <div className="border p-5 rounded-2xl overflow-auto"></div>
+      <div className="border p-5 rounded-2xl overflow-auto bg-slate-100/50"></div>
       {/* CENTER */}
-      <div className="col-span-2 border p-5 rounded-2xl overflow-auto"></div>
+      <div className="col-span-2 border p-5 rounded-2xl overflow-auto bg-slate-100/50"></div>
       {/* RIGHT */}
-      <div className="border p-5 rounded-2xl overflow-auto"></div>
+      <div className="border p-5 rounded-2xl overflow-auto bg-slate-100/50"></div>
     </div>
   );
 }
