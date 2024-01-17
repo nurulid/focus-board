6efbe35177719cbeeb91c0080a3b61ccee3e6363
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-17 20:29:49 +0700

    feat: add digital clock

---
+++ src/components/feature/DigitalClock.jsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+export const DigitalClock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatTime = currentTime.toLocaleTimeString('en-US', {
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  });
+
+  return (
+    <div className="p-4 text-center text-2xl border border-gray-100 bg-white rounded-md tracking-[5px]">{formatTime}</div>
+  ) 
+};
