e6eeeb88174688dbfafd7be142fbc2ad6421de85
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-02-02 16:39:37 +0700

    feat: add timer

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { DigitalClock } from "@/components/feature/DigitalClock";
+import { PomodoroTimer } from "@/components/feature/PomodoroTimer";
 import { AllTasks } from "@/components/tasks/components/AllTasks";
 import { CreateTask } from "@/components/tasks/components/CreateTask";
 import { checkEnvironment } from "@/config/apiUrl";
@@ -12,7 +13,10 @@ async function getTasks() {
 }
 
 export default async function Home() {
-  const { tasks } = await getTasks();
+  const { tasks } = await getTasks();  
+  const time = new Date();
+
+  time.setSeconds(time.getSeconds() + 1500); // 60 * 1500 = 25 minutes timer
 
   return (
     <div className="grid grid-cols-4 gap-6 h-[calc(100vh-220px)]">
@@ -24,8 +28,9 @@ export default async function Home() {
         <AllTasks tasks={tasks} />
       </div>
       {/* RIGHT */}
-      <div className="border p-5 rounded-2xl overflow-auto bg-gray-300/10">
+      <div className="border p-5 rounded-2xl overflow-auto bg-gray-300/10 space-y-5">
         <DigitalClock />
+        <PomodoroTimer expiryTimestamp={time}/>
       </div>
     </div>
   );
